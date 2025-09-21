0699a6c3113672232553d3c093115eb7ce3eb03e
Ivan Ushakov <i.ushakov@g.nsu.ru>
2025-09-21 22:07:39 +0700

    OOPStyle

--- task-0/task-0b/main.cpp
+++ task-0/task-0b/main.cpp
@@ -6,49 +6,95 @@
 #include <map>
 #include <vector>
 
-int main(int argc, char* argv[]){
-    std::string inFileName = argv[1];
-    std::ifstream inputFile(inFileName);
+class TextReader{
+public:
+    explicit TextReader(const std::string& filename) : filename(filename){}
 
-    std::string outputFileName = argv[2];
-    std::ofstream outputFile(outputFileName);
+    std::list<std::string> readLines(){
+        std::ifstream inputFile(filename);
+        std::list<std::string> lines;
+        std::string line;
 
-    std::string line;
-    std::list<std::string> lines;
-    while(getline(inputFile, line)){
-        lines.push_back(line);
+        while(getline(inputFile, line)){
+            lines.push_back(line);
+        }
+        return lines;
     }
-    inputFile.close();
+private:
+    std::string filename;
+};
 
-    std::map<std::string, int> wordCount;
-    int amount = 0;
-    for(const auto& listLine: lines){
-        size_t start = 0;
-        while(start < listLine.size()){
-            size_t first = listLine.find_first_not_of(' ', start);
-            if(first == std::string::npos) break;
+class WordCounter{
+public:
+    void processLines(const std::list<std::string>& lines){
+        for(const auto& line : lines){
+            size_t start = 0;
+            while(start < line.size()){
+                size_t first = line.find_first_not_of(' ', start);
+                if(first == std::string::npos) break;
 
-            size_t last = listLine.find_first_of(' ', first);
-            if(last == std::string::npos){
-                last = listLine.size();
+                size_t last = line.find_first_of(' ', first);
+                if(last == std::string::npos) last = line.size();
+
+                std::string word = line.substr(first, last - first);
+                ++totalWords;
+                ++wordCount[word];
+                start = last;
             }
-            std::string word = listLine.substr(first, last - first);
-            ++amount;
-            ++wordCount[word];
-            start = last;
         }
     }
-    std::cout << std::fixed << std::setprecision(1);
 
-    std::vector<std::pair<std::string, int>> vec(wordCount.begin(), wordCount.end());
-    std::sort(
-            vec.begin(), vec.end(),
-            [](const auto& a, const auto& b){return a.second > b.second;}
-            );
-    outputFile << std::fixed << std::setprecision(1);
-    for(const auto& pair: vec){
-         outputFile << pair.first << ' ' << pair.second << ' ' << ((double)pair.second*100/amount) << "%" << '\n';
+    const std::map<std::string, int>& getWordCount() const{
+        return wordCount;
     }
-    outputFile.close();
+
+    int getTotalWords() const{
+        return totalWords;
+    }
+private:
+    std::map<std::string, int> wordCount;
+    int totalWords = 0;
+};
+
+
+class ResultWriter {
+public:
+    explicit ResultWriter(const std::string& filename) : filename(filename) {}
+
+    void write(const std::map<std::string, int>& wordCount, int totalWords) {
+        std::ofstream outputFile(filename);
+        std::vector<std::pair<std::string, int>> vec(wordCount.begin(), wordCount.end());
+
+        std::sort(vec.begin(), vec.end(),
+                  [](const auto& a, const auto& b) { return a.second > b.second; });
+
+        outputFile << std::fixed << std::setprecision(1);
+        for (const auto& pair : vec) {
+            outputFile << pair.first << ';' << pair.second << ';'
+                       << ((double)pair.second * 100 / totalWords) << "%" << '\n';
+        }
+    }
+
+private:
+    std::string filename;
+};
+
+class Application{
+public:
+    Application(const std::string& input, const std::string& output): reader(input), writer(output) {}
+    void run(){
+        auto lines = reader.readLines();
+        counter.processLines(lines);
+        writer.write(counter.getWordCount(), counter.getTotalWords());
+    }
+private:
+    TextReader reader;
+    WordCounter counter;
+    ResultWriter writer;
+};
+
+int main(int argc, char* argv[]){
+    Application app(argv[1], argv[2]);
+    app.run();
     return 0;
 }
